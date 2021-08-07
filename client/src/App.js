3bfb678ee51e056aa10e24b23f6ebf8fea68b8a4
Scott Geleas <scottgeleas97@gmail.com>
2021-08-07 11:54:18 -0400

    fixed login app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
      <Header />
      <Signup />
-
-
-
-     
+     <Login />     
     </div>
   );
 }
