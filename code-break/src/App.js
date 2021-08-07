9a334e7726900145dcf42f7bf3f69d370053d805
Erica Fagioli <efagioli01@gmail.com>
2021-08-07 11:23:35 -0400

    rough login.js page

--- code-break/src/App.js
+++ code-break/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header.js'
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
      <Header />
+
+
+     <Login />
      
     </div>
   );
