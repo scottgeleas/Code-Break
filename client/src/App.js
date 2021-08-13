a28ac302707c9261a24451dfed461a8cb449fe55
Scott Geleas <scottgeleas97@gmail.com>
2021-08-12 21:29:59 -0400

    basic filter component with div for snippet component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -8,50 +13,63 @@ import Signup from './components/Signup/index';
 import Login from './components/Login/index';
 import Banner from './components/Banner/index';
 import Footer from './components/Footer/index';
+import Filter from './components/Filter/index';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+    uri: '/graphql',
 });
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+    link: httpLink,
+    cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <div className='pageHeader'>
+                        <Header />
+                        <Nav />
+                    </div>
+                    <Banner />
+                    <Switch>
+                        <Route path='/' exact>
+                            <h1>Homepage</h1>
+                        </Route>
+                        <Route path='/dashboard' exact>
+                            <h1>Dashboard</h1>
+                        </Route>
+                        <Route path='/signup' exact>
+                            <Signup />
+                        </Route>
+                        <Route path='/login' exact>
+                            <Login />
+                        </Route>
+                    </Switch>
 
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <div className='pageHeader'>
-            <Header />
-            <Nav />
-          </div>
-          <Banner />
-          <Switch>
-            <Route path='/' exact>
-              <h1>Homepage</h1>
-            </Route>
-            <Route path='/dashboard' exact>
-              <h1>Dashboard</h1>
-            </Route>
-            <Route path='/signup' exact>
-              <Signup />
-            </Route>
-            <Route path='/login' exact>
-              <Login />
-            </Route>
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    </ApolloProvider>
+                    <div className='mainContent'>
+                        <div className='container'>
+                            <div className='row'>
 
-  );
+                                <div className='col-3'>
+                                    <Filter />
+                                </div>
+
+                                <div className='col-9'>
+                                    {/* Snippet Component here */}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <Footer />
+                </div>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;
-
-
-
