3f20c3664b2419f058aa8ee289840f8449c59d41
Marla Jane <marlajanecaram@gmail.com>
2021-08-17 18:37:21 -0400

    started pushing to finish

--- client/src/components/Snippet/index.js
+++ client/src/components/Snippet/index.js
@@ -1,55 +1,21 @@
-import React from 'react';
+// import React, { useState } from 'react';
 // import snippet query from utils/queries.js
+import { useQuery } from '@apollo/client';
+import { GET_ALL_SNIPPETS } from '../../utils/queries';
 
 export default function Snippet(props) {
-    // useQuery to get all snippets (const {loading, data} = useQuery(GET_ALL_SNIPPET);)
+    const {loading, data} = useQuery(GET_ALL_SNIPPETS);
 
-    // console.log(props.selectedLanguage);
+    console.log(props.selectedLanguage);
 
-    //let snippetArray = []
+    const snippetArray = []
 
-    // if (!loading){
-        //let snippetArray = data
+    if (!loading){
+        const snippetArray = data
 
-        // if (props.selectedLanguage) {
-        //     console.log('Use selected language and filter snippets array that store in data variable');
-        //     snippetArray = data.filter()
-        // }
-    // }
-
-    // return (
-    //     {snippetArray.map(item => {
-    //         return (
-    //             <div class="card" id="snippetCard">
-    //                 <div class="card-header">
-    //                     {item.title}
-    //                 </div>
-    //                 <div class="card-body">
-    //                     <p class="card-text">
-    //                             {item.description}
-    //                     </p>
-    //                 </div>
-    //                 <div class="card-footer">
-        
-    //                 </div>
-    //             </div>
-    //         );
-    //     })}
-    // );
-    return (
-        <div class="card" id="snippetCard">
-            <div class="card-header">
-                Snippet Title
-                {props.selectedLanguage}
-            </div>
-            <div class="card-body">
-                <p class="card-text">
-
-                </p>
-            </div>
-            <div class="card-footer">
-
-            </div>
-        </div>
-    );
-}
+        if (props.selectedLanguage) {
+            // console.log('Use selected language and filter snippets array that store in data variable');
+            snippetArray = data.filter()
+        };
+    };
+};
