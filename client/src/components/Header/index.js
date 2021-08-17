e846db6135dae6143380bd74043142e0883b685e
Erica Fagioli <efagioli01@gmail.com>
2021-08-17 19:56:04 -0400

    banner update/media queries

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,7 +7,6 @@ export default function Header() {
     return (
         <div className='container'>
              <img id='logo' src={logo} alt="logo" />
-            {/* <h1 className='code'>Code {`<br>`}</h1> */}
         </div>
     );
 }
