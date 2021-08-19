36d1ed906138af025c04cf736fc4d69ab3dabc63
Oleksandr Kulyk <kylik.aleksandr@gmail.com>
2021-08-19 15:03:13 -0400

    Fix heroku routes.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const startApolloServer = async () => {
     const server = new ApolloServer({
         typeDefs,
