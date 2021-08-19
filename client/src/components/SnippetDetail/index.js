7d4f7217b628c8177e302d8c58b949eeb64c2c75
Marla Jane <marlajanecaram@gmail.com>
2021-08-18 21:23:16 -0400

    testing some differnt styles

--- client/src/components/SnippetDetail/index.js
+++ client/src/components/SnippetDetail/index.js
@@ -1,5 +1,7 @@
 import hljs from 'highlight.js';
 import 'highlight.js/styles/vs2015.css';
+import './css/index.css';
+
 
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
@@ -8,6 +10,7 @@ import { GET_SNIPPET } from '../../utils/queries';
 import Auth from '../../utils/auth';
 import CommentForm from '../CommentForm/index';
 import CommentsList from '../CommentsList/index';
+import Filter from '../Filter/index'
 
 function SnippetDetail() {
     const { snippetId } = useParams();
@@ -33,34 +36,43 @@ function SnippetDetail() {
     }
 
     return (
-        <div className="container-xxl snippet-detail">
+        
+        <div className="container snippet-detail">
             {loading ? (
                 <div className="spinner-border text-primary" role="status">
                     <span className="visually-hidden">Loading...</span>
                 </div>
             ) : (
-                <>
-                    <h2 className="display-3">{data.getSnippet.title}</h2>
-                    <p className="lead mb-3">{data.getSnippet.description}</p>
-                    <div className="card mb-5">
-                        <div className="card-header">
-                            {data.getSnippet.language}
-                        </div>
-                        <div className="card-body p-0">
-                            <pre className="m-0"><code className={'hljs language-' + hljsData.language.toLowerCase()} dangerouslySetInnerHTML={{__html: hljsData.value}}></code></pre>
-                        </div>
-                        <div class="card-footer">
-                            <i class="bi bi-hand-thumbs-up-fill"></i>{data.getSnippet.like} <i class="bi bi-hand-thumbs-down-fill"></i>{data.getSnippet.dislike}
+                <div class="row">
+                    {/* <div class="col s12 m4" id="filter">
+                        <Filter />
+                    </div> */}
+                    <div class="col s12 m7">
+                        <h2 className="display-3" id="snippet-title">{data.getSnippet.title}</h2>
+                        <p className="lead mb-3" id="snippet-description">{data.getSnippet.description}</p>
+                        <div className="card mb-5" id="snippet-card">
+                            <div className="card-header">
+                                {data.getSnippet.language}
+                            </div>
+                            <div className="card-body p-0">
+                                <pre className="m-0"><code className={'hljs language-' + hljsData.language.toLowerCase()} dangerouslySetInnerHTML={{__html: hljsData.value}}></code></pre>
+                            </div>
+                            <div class="card-footer">
+                                <i class="bi bi-hand-thumbs-up-fill"></i>{data.getSnippet.like} <i class="bi bi-hand-thumbs-down-fill"></i>{data.getSnippet.dislike}
+                            </div>
                         </div>
                     </div>
-                    {isLoggedIn ? (
-                        <CommentForm />
-                    ) : null}
-                    <CommentsList comments={data.getSnippet.comments}/>
-                </>
+                    <div class="col s12 m5" tabIndex="0" id="comments">    
+                        {isLoggedIn ? (
+                            <CommentForm />
+                        ) : null}
+                        <CommentsList comments={data.getSnippet.comments}/>
+                    </div>
+                </div>
             )}
         </div>
     );
 }
+
 
 export default SnippetDetail;
